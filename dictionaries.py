25bc4d2a30fe0f99f3c8fb2393206dfee2f7bd63
muhammad-hamza-shahid <50265613+muhammad-hamza-shahid@users.noreply.github.com>
2020-06-03 14:13:57 +0500

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -5,6 +5,6 @@ print(myDict["phone"][1])
 
 myDict1={"class":8,"names":{1:"hamza",2:"humair",3:"Irsa"}}
 print(myDict1["names"][2])
-myDict2={"class":9,"subjects":[{'english1':'a','english2':'b'},'urdu','islamiyat']}
+myDict2={"class":9,"subjects":[('english1','a','english2','b'),'urdu','islamiyat']}
 print(myDict2['subjects'][2])
-print(myDict2['subjects'][0]['english2'])
+print(myDict2['subjects'][0][2])
