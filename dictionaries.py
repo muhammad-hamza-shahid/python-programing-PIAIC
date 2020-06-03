78bd4d2a7d61e0d3cc92319f4439be0cff95a1b7
muhammad-hamza-shahid <50265613+muhammad-hamza-shahid@users.noreply.github.com>
2020-06-03 14:12:20 +0500

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,10 @@
+myDict={'name':'hamza','rollno':1582,'email':'mr.hamza.shahid@gmail.com','phone':['03438559089','03435630466']}
+print(myDict)
+
+print(myDict["phone"][1])
+
+myDict1={"class":8,"names":{1:"hamza",2:"humair",3:"Irsa"}}
+print(myDict1["names"][2])
+myDict2={"class":9,"subjects":[{'english1':'a','english2':'b'},'urdu','islamiyat']}
+print(myDict2['subjects'][2])
+print(myDict2['subjects'][0]['english2'])
