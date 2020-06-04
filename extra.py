f2f8bb3bb6d96422459a4f21f7f29a22b5817333
muhammad-hamza-shahid <50265613+muhammad-hamza-shahid@users.noreply.github.com>
2020-06-04 23:36:59 +0500

    Create extra.py

---
+++ extra.py
@@ -0,0 +1,12 @@
+print(2 ** 3 ** 2 ** 1)
+
+print("peter's sister's name's \"Anna\"")
+print('peter\'s sister\'s name\'s "Anna"')
+
+i=250
+print(len(str(i)))
+while len(str(i)) >72:
+    i *= 2
+else:
+    i//=2
+print(i)
