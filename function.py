8c53e7f626feb8083c672a116126ab54197640f7
muhammad-hamza-shahid <50265613+muhammad-hamza-shahid@users.noreply.github.com>
2020-06-12 23:38:13 +0500

    Update function.py

--- function.py
+++ function.py
@@ -6,3 +6,17 @@ b=input("enter second digit \n")
 print("Total sum is :")
 sum(a,b)
 print("hello")
+#Passing value to positional function
+
+def pos_fun(a,b):
+    print(a+b)
+pos_fun(b=3,a=22)
+
+#function having default values
+
+def def_val(a=2,b=2):
+    print(a+b)
+
+def_val(3)
+def_val(b=22)
+
